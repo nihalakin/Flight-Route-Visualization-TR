e67914825c14da2cee3d446d07c213e3f0233b5a
nihalakin <nihalakin222@gmail.com>
2026-03-19 02:10:52 +0300

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+from app.models.user import User
+from app.models.ticket import Ticket
+from app.models.ticket_detail import TicketDetail
+from app.models.airline import Airline
+from app.models.user_review import UserReview
+from app.models.coupon import Coupon
+from app.models.airport import Airport
+
+__all__ = ["User", "Ticket", "TicketDetail", "Airline", "UserReview", "Coupon", "Airport"]
