0b9327dd9ddc005cd1ebc041bf6bfa847e966b21
nihalakin <nihalakin222@gmail.com>
2026-05-10 13:46:03 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,15 +4,10 @@ from app.models.ticket_detail import TicketDetail
 from app.models.ticket_segment import TicketSegment
 from app.models.comment import Comment
 from app.models.airline import Airline
+from app.models.user_review import UserReview
 from app.models.coupon import Coupon
 from app.models.airport import Airport
 from app.models.password_reset import PasswordReset
-from app.models.annual_statistics import (
-    AnnualAirTraffic,
-    AnnualCargoTraffic,
-    AnnualPassengerTraffic,
-    AnnualFreightTraffic,
-)
 
 __all__ = [
     "User",
@@ -21,11 +16,8 @@ __all__ = [
     "TicketSegment",
     "Comment",
     "Airline",
+    "UserReview",
     "Coupon",
     "Airport",
     "PasswordReset",
-    "AnnualAirTraffic",
-    "AnnualCargoTraffic",
-    "AnnualPassengerTraffic",
-    "AnnualFreightTraffic",
 ]
